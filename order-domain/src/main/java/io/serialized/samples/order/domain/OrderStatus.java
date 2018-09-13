d452cd1c4fe09f230fbac71b6bafa6302d274745
Jesper Hammarbäck <jesper@hammarback.eu>
2018-09-13 08:51:17 +0200

    Minor change of error message.

--- order-domain/src/main/java/io/serialized/samples/order/domain/OrderStatus.java
+++ order-domain/src/main/java/io/serialized/samples/order/domain/OrderStatus.java
@@ -5,15 +5,15 @@ public enum OrderStatus {
   NEW, PLACED, CANCELLED, PAID, SHIPPED;
 
   void assertNotYetPlaced() {
-    if (NEW != this) throw new IllegalOrderStateException("Expected order to be NEW! was" + this);
+    if (NEW != this) throw new IllegalOrderStateException("Expected order to be NEW but was " + this);
   }
 
   void assertPlaced() {
-    if (PLACED != this) throw new IllegalOrderStateException("Expected order to be PLACED! was: " + this);
+    if (PLACED != this) throw new IllegalOrderStateException("Expected order to be PLACED but was " + this);
   }
 
   void assertPaid() {
-    if (PAID != this) throw new IllegalOrderStateException("Expected order to be PAID! was: " + this);
+    if (PAID != this) throw new IllegalOrderStateException("Expected order to be PAID but was " + this);
   }
 
 }
